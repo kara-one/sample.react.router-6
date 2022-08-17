0ddd12059f03f4ce671579ac893f4bf7d2b551c9
ksawl <ksawl@narod.ru>
2022-08-17 19:39:42 +0300

    fix 4

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import './App.css';
 import { Link, useRoutes } from 'react-router-dom';
 import { About, Home, Users } from './Components';
